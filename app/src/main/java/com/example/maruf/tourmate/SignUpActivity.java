5bf31b106ee81f07ea95888dcc8456f6c50b1726
islamMaruf <marufislam7424@gmail.com>
2018-12-12 03:21:44 +0600

    fire base add to the project

--- app/src/main/java/com/example/maruf/tourmate/SignUpActivity.java
+++ app/src/main/java/com/example/maruf/tourmate/SignUpActivity.java
@@ -1,13 +1,26 @@
 package com.example.maruf.tourmate;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private TextView loginTv ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        loginTv = findViewById(R.id.link_login);
+        loginTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
