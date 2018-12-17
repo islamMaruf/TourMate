8e0c38210a61c78b046b8ce2741f5a433ffae695
islamMaruf <marufislam7424@gmail.com>
2018-12-17 20:16:32 +0600

    memerable place added

--- app/src/main/java/com/example/maruf/tourMateApplication/Fragments/MemorablePlacesFragment.java
+++ app/src/main/java/com/example/maruf/tourMateApplication/Fragments/MemorablePlacesFragment.java
@@ -2,7 +2,6 @@ package com.example.maruf.tourMateApplication.Fragments;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
@@ -15,7 +14,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.ByteArrayOutputStream;
-import java.util.concurrent.TimeUnit;
+
 
 import es.dmoral.toasty.Toasty;
 
@@ -36,6 +35,26 @@ public class MemorablePlacesFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try{
+            if (requestCode == 1 && resultCode == Activity.RESULT_OK){
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG,100,byteArrayOutputStream);
+                byte[] uri = byteArrayOutputStream.toByteArray();
+
+                final StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("MemorablePhotos").child("photos_"+System.currentTimeMillis());
+                storageReference.putBytes(uri);
+
+                Toast.makeText(getActivity(), "picture add", Toast.LENGTH_SHORT).show();
+            }
+
+        }catch (Exception e){
+            Toasty.error(this.getActivity(),e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
     }
 
 
